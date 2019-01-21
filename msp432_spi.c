3a19739771a44358135263738c5a25f7ef5499c9
Michelle <michelle@Michelles-MacBook-Pro.local>
2019-01-21 16:54:52 -0500

    updated

--- msp432_spi.c
+++ msp432_spi.c
@@ -12,39 +12,57 @@
 char cmdbuf[20];
 char cmd_index=0;
 
+/** Delay function. **/
+void delay(unsigned int d) {
+  int i;
+  for (i = 0; i<d; i++) {
+    __no_operation();
+  }
+}
+
+void flash_spi_detected(void) {
+    int i=0;
+    P1OUT = 0;
+    for (i=0; i < 6; ++i) {
+        P1OUT = ~P1OUT;
+        delay(0x4fff);
+        delay(0x4fff);
+    }
+}
+
 void main(void) {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	
 	P1DIR |= BIT0;
-	while (P1IN & BIT5);                   // If clock sig from mstr stays low,
-	                                            // it is not yet in SPI mode
+	while (P1IN & BIT6);                   // If clock sig from mstr stays low, it is not yet in SPI mode
 	flash_spi_detected();                 // Blink 3 times
 	
-	P1SEL0 = BIT5 + BIT6 + BIT7;     // clk + SIMO + SOMI
-	P1SEL1 = BIT5 + BIT6 + BIT7;    // clk + SIMO + SOMI
+	P2SEL0 = BIT3 + BIT2;       // SIMO + SOMI
+	P2SEL1 = BIT3 + BIT2;       // SIMO + SOMI
+
+	P1SEL0 = BIT6;              // SDA
+	P1SEL1 = BIT6;              // SDA
 	
-	UCB0CTLW0 = UCSWRST;            // Enable software reset
-	UCB0CTLW0 |= UCMSB + UCSYNC;    // 3-pin, 8-bit, most sig bit, synchronous mode
-	UCB0CTLW0 &= ~UCSWRST;
+	UCA1CTLW0 = UCSWRST;            // Enable software reset
+	UCA1CTLW0 |= UCMSB + UCSYNC;    // 3-pin, 8-bit, most sig bit, synchronous mode
+	UCA1CTLW0 &= ~UCSWRST;
 	
-	UCB0IE |= UCRXIE;                // Enable RX interrupt
+	UCA1IE |= UCRXIE;                // Enable RX interrupt
 	
-	__enable_interrupt();           // Enable interrupts
+	__enable_irq();
+	NVIC->ISER[0] = 1 << ((EUSCIB0_IRQn) & 31);           // Enable interrupts
+	while(1);
 }
 
-//__attribute__((interrupt(USCIAB0RX_VECTOR))) void USCI0RX_ISR (void) {
-//    
-//}
-
-#pragma vector = eUSCI_B0_VECTOR
-__interrupt void eUSCI_B0_ISR() {
-    char value = UCB0RXBUF;
+void EUSCIB0_IRQHandler() {
+    char value = UCA1RXBUF;
     if (value == '\n') {
         if (strncmp(cmdbuf, "HELLO WORLD", 11) == 0) {
             P1OUT |= BIT0;
-        } else {
-            P1OUT &= ~BIT0;
         }
+//        } else {
+//            P1OUT &= ~BIT0;
+//        }
         cmd_index = 0;
     } else {
         cmdbuf[cmd_index] = value;
