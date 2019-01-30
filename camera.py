3db12c127c583175467ff718d82dd099b0952972
jennyt96 <jennytong@ufl.edu>
2019-01-30 00:09:36 -0500

    Updated camera script to do video and picture

--- camera.py
+++ camera.py
@@ -1,9 +1,20 @@
 from picamera import PiCamera
 from time import sleep
+import sys
+
+mode = sys.argv[1]
+output_name = sys.argv[2]
 
 camera = PiCamera()
 
 camera.start_preview()
-camera.capture('/home/pi/Desktop/Images/test_img.jpg')
-sleep(5)
+
+if mode == 'picture':
+	camera.capture('/home/pi/Desktop/Images/' + output_name)
+	sleep(5)
+elif mode == 'video':
+	camera.start_recording('/home/pi/Desktop/Videos/' + output_name)
+	sleep(10)
+	camera.stop_recording()
+
 camera.stop_preview()
