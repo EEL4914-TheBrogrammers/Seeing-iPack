1a2ffccb5301ec8f8a7c4aa821a96c7bdcede5c5
Jenny <jennytong@ufl.edu>
2019-04-08 13:52:27 -0400

    Updated Lidar to use EUSCIB1 instead of EUSCIB0

--- lidar.c
+++ lidar.c
@@ -1,32 +1,32 @@
+/*
+ * lidar.c
+ */
+
 #include "msp.h"
 #include <lidar.h>
 #include <stdint.h>
 #include <string.h>
 
-// *******************************   NOTES    ***********************************
-
-
-// ******************************   Functions    *********************************
 // Manually toggle SCL pin to allow slave to release SDA
 void manual_reset() {
     volatile uint32_t i;
 
     // Change P1.7 from SCL to GPIO pin
-    P1->SEL0 &= ~BIT7;
-    P1->DIR |= BIT7;
+    P6->SEL0 &= ~BIT5;
+    P6->DIR |= BIT5;
 
     for(i = 0; i < 9; i++) {
-        __delay_cycles(4);
-        P1->OUT |= BIT7;
+        __delay_cycles(1);
+        P6->OUT |= BIT5;
 
-        __delay_cycles(4);
-        P1->OUT &= ~BIT7;
+        __delay_cycles(1);
+        P6->OUT &= ~BIT5;
     }
 
     // Change P1.7 from GPIO to SCL pin
-    P1->OUT |= BIT7;
-    P1->DIR &= ~BIT7;
-    P1->SEL0 |= BIT7;
+    P6->OUT |= BIT5;
+    P6->DIR &= ~BIT5;
+    P6->SEL0 |= BIT5;
 }
 
 // Delay function in ms for 30 MHz clock
@@ -46,13 +46,13 @@ void i2c_tx(short address, short data) {
 
     counter = 1;
 
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR + EUSCI_B_CTLW0_TXSTT;  // Transmit start condition
+    EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TR + EUSCI_B_CTLW0_TXSTT;  // Transmit start condition
 
-    while ((EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTT));
-    while (!(EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTP));           // Ensure stop condition got sent
-    while (EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0);
+    while ((EUSCI_B1->CTLW0 & EUSCI_B_CTLW0_TXSTT));
+    while (!(EUSCI_B1->CTLW0 & EUSCI_B_CTLW0_TXSTP));           // Ensure stop condition got sent
+    while (EUSCI_B1->IFG & EUSCI_B_IFG_TXIFG0);
 
-    for (i = 400; i > 0; i--);
+    for (i = 40; i > 0; i--);
 }
 
 // I2C receive function
@@ -62,47 +62,53 @@ unsigned short i2c_rx(short address) {
 
     counter = 4;
 
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TR + EUSCI_B_CTLW0_TXSTT;  // Transmit start condition
+    EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TR + EUSCI_B_CTLW0_TXSTT;  // Transmit start condition
 
-    while ((EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTT));
-    while (!(EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTP));           // Ensure stop condition got sent
-    while (EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0);
+    while ((EUSCI_B1->CTLW0 & EUSCI_B_CTLW0_TXSTT));
+    while (!(EUSCI_B1->CTLW0 & EUSCI_B_CTLW0_TXSTP));           // Ensure stop condition got sent
+    while (EUSCI_B1->IFG & EUSCI_B_IFG_TXIFG0);
 
-    for (i = 400; i > 0; i--);
+    for (i = 40; i > 0; i--);
 
     counter = 6;
 
-    EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_TR;           //Set Receiver bit
-    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTT;         //Set start bit
+    EUSCI_B1->CTLW0 &= ~EUSCI_B_CTLW0_TR;           //Set Receiver bit
+    EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TXSTT;         //Set start bit
 
-    while ((EUSCI_B0->CTLW0 & EUSCI_B_CTLW0_TXSTT));
-    while (EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG0);
+    while ((EUSCI_B1->CTLW0 & EUSCI_B_CTLW0_TXSTT));
+    while (EUSCI_B1->IFG & EUSCI_B_IFG_RXIFG0);
 
     counter = 0;
 
-    for (i = 400; i > 0; i--);
+    for (i = 40; i > 0; i--);
 
     return lvalue;
 }
 
 // I2C initialization function
 void i2c_init() {
-    P1->SEL0 |= BIT6 | BIT7;                    // I2C pins (P1.6 SDA, P1.7 SCL)
+    P6->SEL0 |= BIT4 | BIT5;                    // I2C pins (P1.6 SDA, P1.7 SCL)
+
+    // Enable global interrupt
+    __enable_irq();
 
     // Enable eUSCIB0 interrupt in NVIC module
-    NVIC->ISER[0] = 1 << ((EUSCIB0_IRQn) & 31);
+    NVIC->ISER[0] = 1 << ((EUSCIB1_IRQn) & 31);
 
     // Configure USCI_B0 for I2C mode
-    EUSCI_B0->CTLW0 |= EUSCI_A_CTLW0_SWRST;     // Software reset enabled
-    EUSCI_B0->CTLW0 = EUSCI_A_CTLW0_SWRST |     // Remain eUSCI in reset mode
+    EUSCI_B1->CTLW0 |= EUSCI_A_CTLW0_SWRST;     // Software reset enabled
+    EUSCI_B1->CTLW0 = EUSCI_A_CTLW0_SWRST |     // Remain eUSCI in reset mode
             EUSCI_B_CTLW0_MODE_3 |              // I2C mode
             EUSCI_B_CTLW0_MST |                 // Master mode
             EUSCI_B_CTLW0_SYNC |                // Sync mode
             EUSCI_B_CTLW0_SSEL__SMCLK;          // SMCLK
 
-    EUSCI_B0->BRW = 120;                        // Baud rate = SMCLK / 30 = 100kHz
-    EUSCI_B0->I2CSA = SLAVE_ADDR;               // Slave address
-    EUSCI_B0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST;    // Release eUSCI from reset
+    EUSCI_B1->BRW = 30;                         // Baud rate = SMCLK / 30 = 100kHz
+    EUSCI_B1->I2CSA = SLAVE_ADDR;               // Slave address
+    EUSCI_B1->CTLW0 &= ~EUSCI_A_CTLW0_SWRST;    // Release eUSCI from reset
+
+    EUSCI_B1->IE |= EUSCI_B_IE_RXIE |           // Enable RX and TX interrupt
+            EUSCI_B_IE_TXIE;
 
     avg_count = 0;
 }
@@ -112,9 +118,7 @@ float get_distance(char* type) {
     float average_cm;
     float average_in;
 
-    while (avg_count < SAMPLES) {
-        EUSCI_B0->IE |= EUSCI_B_IE_RXIE |           // Enable RX and TX interrupt
-                                    EUSCI_B_IE_TXIE;
+    while (avg_count <= SAMPLES) {
         i2c_tx(ACQ_COMMAND, START_MEASURE);     // (TX) Take distance measurement w/ receiver bias
         i2c_rx(DISTANCE);                       // (TX + RX) Receive distance measurement
         i2c_tx(ACQ_COMMAND, RESET);             // (TX) Reset all registers to default values
@@ -141,54 +145,54 @@ float get_distance(char* type) {
 }
 
 // I2C interrupt service routine
-void EUSCIB0_IRQHandler(void)
+void EUSCIB1_IRQHandler(void)
 {
-    if (!(EUSCI_B0->IFG & EUSCI_B_IFG_NACKIFG)) {
+    if (!(EUSCI_B1->IFG & EUSCI_B_IFG_NACKIFG)) {
         switch(counter){
             case 1:{    // Transmit register address
-                EUSCI_B0->TXBUF = tx_addr;
+                EUSCI_B1->TXBUF = tx_addr;
                 counter++;
                 break;
             }
             case 2:{    // Transmit data
-                EUSCI_B0->TXBUF = tx_data;
+                EUSCI_B1->TXBUF = tx_data;
                 counter++;
                 break;
             }
             case 3:{    // Transmit stop bit and clear TXIFG
-                EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
-                EUSCI_B0->IFG &= ~EUSCI_B_IFG_TXIFG;
+                EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+                EUSCI_B1->IFG &= ~EUSCI_B_IFG_TXIFG;
                 break;
             }
             case 4:{    // Transmit register address to receive
-                if(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0){
-                    EUSCI_B0->TXBUF = tx_addr;
+                if(EUSCI_B1->IFG & EUSCI_B_IFG_TXIFG0){
+                    EUSCI_B1->TXBUF = tx_addr;
                     counter++;
                 }
                 break;
             }
             case 5:{    // Transmit stop bit and clear TXIFG to receive
-                if(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG0){
-                    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
-                    EUSCI_B0->IFG &= ~EUSCI_B_IFG_TXIFG;
+                if(EUSCI_B1->IFG & EUSCI_B_IFG_TXIFG0){
+                    EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+                    EUSCI_B1->IFG &= ~EUSCI_B_IFG_TXIFG;
                 }
                 break;
             }
             case 6:{    // Receive distance measurement high byte
-                if(EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG0){
-                    hvalue = EUSCI_B0->RXBUF;
+                if(EUSCI_B1->IFG & EUSCI_B_IFG_RXIFG0){
+                    hvalue = EUSCI_B1->RXBUF;
                 }
                 counter++;
                 break;
             }
             default:{   // Receive distance measurement low byte, transmit stop bit, and clear RX and TX flags
-                if(EUSCI_B0->IFG & EUSCI_B_IFG_RXIFG0){
-                    lvalue = EUSCI_B0->RXBUF;
+                if(EUSCI_B1->IFG & EUSCI_B_IFG_RXIFG0){
+                    lvalue = EUSCI_B1->RXBUF;
                 }
 
-                EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
-                EUSCI_B0->IFG &= ~EUSCI_B_IFG_TXIFG;
-                EUSCI_B0->IFG &= ~EUSCI_B_IFG_RXIFG;
+                EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+                EUSCI_B1->IFG &= ~EUSCI_B_IFG_TXIFG;
+                EUSCI_B1->IFG &= ~EUSCI_B_IFG_RXIFG;
                 break;
             }
         }
