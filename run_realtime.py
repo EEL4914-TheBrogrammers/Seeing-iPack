39a89a4fb97e51273f7842c07902000672b47191
jennyt96 <jennytong@ufl.edu>
2019-04-09 17:38:52 -0400

    changed run_realtime to take pictures and process per frame

--- run_realtime.py
+++ run_realtime.py
@@ -49,6 +49,10 @@ def main():
 		
 		rawCapture.seek(0)
 		rawCapture.truncate()
+
+		print (image.shape)
+		img_og = cv2.imread("bleh/frame1.jpg")
+		print (img_og.shape)
 		
 		img_og, img_threshold = draw_contour_main_realtime(image)
 		lane_img = img_pipeline_main(img_og, img_threshold)
