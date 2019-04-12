67f9c18d4513d939e7bdfcd8dc5d96471963b5d0
Jenny <jennytong@ufl.edu>
2019-04-11 20:56:29 -0400

    added a start if condition in spi interrupt

--- spi_msp.c
+++ spi_msp.c
@@ -7,54 +7,60 @@
 
 // SPI initialization
 void spi_init() {
-    while (P6IN & BIT3);                        // If clock sig from mstr stays low, it is not yet in SPI mode
+    while (P1IN & BIT5);                        // If clock sig from mstr stays low, it is not yet in SPI mode
 
-    P6->SEL0 |= BIT3 | BIT4 | BIT5;             // P6.3 Clk, P1.6 SIMO, P1.7 SOMI
+    P1->SEL0 |= BIT5 | BIT6 | BIT7;             // P1.5 Clk, P1.6 SIMO, P1.7 SOMI
 
-    P5->DIR |= BIT4 | BIT5;
+    P5->DIR |= BIT3 | BIT2;
+    P5->OUT &= ~BIT3;
+    P5->OUT &= ~BIT2;
 
-    EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_SWRST;     // Put eUSCI state machine in reset
-    EUSCI_B1->CTLW0 = EUSCI_B_CTLW0_SWRST |     // Remain eUSCI state machine in reset
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SWRST;     // Put eUSCI state machine in reset
+    EUSCI_B0->CTLW0 = EUSCI_B_CTLW0_SWRST |     // Remain eUSCI state machine in reset
                 EUSCI_B_CTLW0_SYNC |            // Set as synchronous mode
                 EUSCI_B_CTLW0_CKPL |            // Set clock polarity high
                 EUSCI_B_CTLW0_MSB;              // MSB first
 
-    EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_SSEL__ACLK;// ACLK
-    EUSCI_B1->BRW = 0x01;                       // /2,fBitClock = fBRCLK/(UCBRx+1).
-    EUSCI_B1->CTLW0 &= ~EUSCI_B_CTLW0_SWRST;    // Initialize USCI state machine
+    EUSCI_B0->CTLW0 |= EUSCI_B_CTLW0_SSEL__ACLK;// ACLK
+    EUSCI_B0->BRW = 0x01;                       // /2,fBitClock = fBRCLK/(UCBRx+1).
+    EUSCI_B0->CTLW0 &= ~EUSCI_B_CTLW0_SWRST;    // Initialize USCI state machine
 
     cmd_index = 0;
+    start = 0;
 }
 
 // SPI interrupt initialization
 void spi_interrupt_init(){
-    EUSCI_B1->IE |= EUSCI_B_IE_RXIE;            // Enable USCI_B0 RX interrupt
+    EUSCI_B0->IE |= EUSCI_B_IE_RXIE;            // Enable USCI_B0 RX interrupt
 
     __enable_irq();                             // Global interrupt
 
     // Enable eUSCI_B0 interrupt in NVIC module
-    NVIC->ISER[0] = 1 << ((EUSCIB1_IRQn) & 31); // Enable interrupts
+    NVIC->ISER[0] = 1 << ((EUSCIB0_IRQn) & 31); // Enable interrupts
 }
 
-void EUSCIB1_IRQHandler() {
-    char value = UCB1RXBUF >> 1;
+void EUSCIB0_IRQHandler() {
+    char value = UCB0RXBUF >> 1;
 
     if (value == '\n') {
         if (strncmp(cmdbuf, "0", 1) == 0) {         // Left trigger
-            P5OUT |= BIT4;
-            P5OUT &= ~BIT5;
+            P5OUT |= BIT3;
+            P5OUT &= ~BIT2;
         } else if (strncmp(cmdbuf, "1", 1) == 0) {  // Right trigger
-            P5OUT |= BIT5;
-            P5OUT &= ~BIT4;
+            P5OUT |= BIT2;
+            P5OUT &= ~BIT3;
+        } else if (strncmp(cmdbuf, "2", 1) == 0) {  // Stop buzzers
+            P5OUT &= ~BIT3;
+            P5OUT &= ~BIT2;
+            start = 0;
+        } else if (strncmp(cmdbuf, "3", 1) == 0) {  // Start program
+            start = 1;
         }
-//        else if (strncmp(cmdbuf, "2", 1) == 0) {  // On trigger
-//            P5OUT |= BIT6;                          // Place holder, do something
-//        }
         cmd_index = 0;
     } else {
         cmdbuf[cmd_index] = value;
         cmd_index++;
     }
 
-    while (!(EUSCI_B1->IFG & EUSCI_B_IFG_TXIFG));
+    while (!(EUSCI_B0->IFG & EUSCI_B_IFG_TXIFG));
 }
