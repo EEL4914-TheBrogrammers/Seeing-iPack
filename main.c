04cd9bce14cf28cb84174a84bae0832727eb20b0
Michelle <michelle@Michelles-MacBook-Pro-2.local>
2019-04-14 20:05:53 -0400

    04/14 fixed button errors, working

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <spi_msp.h>
 #include "msp.h"
 #include "tts.h"
 #include "lidar.h"
-//#include "spi_msp.h"
-//#include "ultrasonic.h"
+#include "spi_msp.h"
 #include "ultra.h"
-//#include "ultra_no_int"
 #include "msp432p401r.h"
 #include "msp432p401r_classic.h"
 
-// **************************   NOTES    ******************************
+/**************************************************
+ *                                                *
+ *                    NOTES                       *
+ *                                                *
+ **************************************************/
 //  -- All messages for speak() must end with a '.'
 //  -- EMIC functionality messes with sensors --> fixed by changing clock divider for ultrasonics and LiDAR
+//  -- Delay after LiDAR readings vary directly with distance of readings (increase delay if increasing bounds)
 //  -- Pins layout:
 //      - Text-to-Speech (UART)
 //          SOUT = P1.2 (RXD)
@@ -34,79 +37,74 @@
 //      - LiDAR sensor (USCI B1 I2C)
 //          SDA = P6.4
 //          SCL = P6.5
-//  -- Delay after LiDAR readings vary directly with distance of readings (increase delay if increasing bounds)
 
-//int arr[5];
-//int c = 0;
 
-//void checkBoundaries(void) {
-//    if (middle_val <= bound && middle_val > 0) {
-//            speak("mid.");
-//    }
-//    if(left_val <= side_bound*2 && left_val > 0) {
-//        speak("left.");
-//    }
-//    if(right_val <= side_bound*2 && right_val > 0) {
-//        speak("right.");
+/**************************************************
+ *                                                *
+ *              GLOBAL VARIABLES                  *
+ *                                                *
+ **************************************************/
+float dist = 0;
+
+/**************************************************
+ *                                                *
+ *                 FUNCTIONS                      *
+ *                                                *
+ **************************************************/
+//void check_bounds(float distance, char* string) {
+//    if(distance < bounds && distance > 0) {
+//        speak(string);
 //    }
 //}
 
-float middle_val = 0;
-
 void main(void) {
-
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
-//    bound = 20;
-//    side_bound = 10;
-//  go = 0;
-//  spi_init();
-//  spi_interrupt_init();
-//
-//  // Wait for on trigger from RPi
-//  while (1) {
-//      if (go == 1) {
-//          break;
-//      }
-//  }
-    speak("Hello. How are you?");
-//    ultra_init();   // Initialize ultrasonic sensors
-//    i2c_init();
-//    manual_reset();
-//    __delay_cycles(10000000);
-//    speak("Setup complete.");
-//    initUltra();
+
+    // Welcome messages
+    speak("Yaaaaas queen. Let's get that shmoney.");
+//    speak("Hello! Welcome to Seeing eye pack.");
+//    speak("Now configuring....");
+
+    // SPI configuration for RPi
+    spi_init();
+    spi_interrupt_init();
+
     while(1) {
+        // Wait on RPi
+        while(start == 0);
+
+        // Get distance measurement from left sensor
         initUltra(LEFT);
-        float dist = find_distance();
+        dist = find_distance();
         __no_operation();
-        if(dist < 30 && dist > 0) {
+//        check_bounds(dist, "left.");
+        if(dist < 30 && dist > 0 && stop == 0) {
             speak("left.");
         }
         dist = 0;
+
+        // Get distance measurement from right sensor
         initUltra(RIGHT);
         dist = find_distance();
-        if(dist < 30 && dist > 0) {
+//        check_bounds(dist, "right.");
+        if(dist < 30 && dist > 0 && stop == 0) {
             speak("right.");
         }
         dist = 0;
-//        ultra_sensors();                        // Get ultrasonic distances
-//        ultra_init();
+
         int i = 0;
         int sumTemp = 0;
         i2c_init();
         for(i = 0; i < 5; i++) {
-//            arr[c] = get_distance("in");
-//            sumTemp += arr[c];
-//            c++;// Get lidar distances
             sumTemp += get_distance("in");
             EUSCI_B0->IE &= ~EUSCI_B_IE_RXIE + ~EUSCI_B_IE_TXIE;       // Disable RX and TX interrupt for I2C
-            __delay_cycles(60000);
+            __delay_cycles(120000);
         }
-        middle_val = sumTemp/5;
-        if(middle_val < 50 && middle_val > 0) {
+        dist = sumTemp/5;
+//        check_bounds(dist, "middle.");
+        if(dist < 50 && dist > 0 && stop == 0) {
             speak("middle.");
         }
-//        left_val = 0;
-        middle_val = 0;
+        dist = 0;
     }
 }
